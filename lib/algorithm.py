d20ad6e08be13a6a183494366a8e9216da04faf2
Arata Jingu <guujin0120@gmail.com>
2018-10-23 00:28:09 +0900

    Implement framework

--- lib/algorithm.py
+++ lib/algorithm.py
@@ -6,13 +6,12 @@ from lib.common import calculate_squared_distance
 def calculate_nearest_neighbour(contour_vertex_positions, body_part_positions):
     nearest_body_part_indices = []
 
-    for i in range(len(contour_vertex_positions)):
-        contour_vertex_position = contour_vertex_positions[i]
+    for _, contour_vertex_position in enumerate(contour_vertex_positions):
         tmp = sys.maxsize
         nearest_body_part_index = None
 
-        for j in range(len(body_part_positions)):
-            squared_distance = calculate_squared_distance(contour_vertex_position, body_part_positions[j])
+        for j, body_part_position in enumerate(body_part_positions):
+            squared_distance = calculate_squared_distance(contour_vertex_position, body_part_position)
 
             if tmp < squared_distance:
                 continue
@@ -25,7 +24,30 @@ def calculate_nearest_neighbour(contour_vertex_positions, body_part_positions):
     return nearest_body_part_indices
 
 
-def calculate_nearest_neighbour_within_contour(img, contour_vertex_positions, body_part_positions):
-    nearest_body_part_indices = []
+def calculate_nearest_neighbour_within_contour(binary_img, contour_vertex_positions, body_part_positions):
+    black_pixels_body_part_dict = {}  # must find using some ways
+    remaining_pixels_body_part_dict = black_pixels_body_part_dict.copy()
+    number_of_remaining_pixels = len(remaining_pixels_body_part_dict)
+
+    for i, body_part_position in enumerate(body_part_positions):
+        black_pixels_body_part_dict[body_part_position] = i
+
+    while number_of_remaining_pixels > 0:
+        for pixel_position in remaining_pixels_body_part_dict.keys():
+            # if up
+            #   number_of_remaining_pixels -= 1
+            #   continue
+            # if left
+            #   number_of_remaining_pixels -= 1
+            #   continue
+            # if down
+            #   number_of_remaining_pixels -= 1
+            #   continue
+            # if right
+            #   number_of_remaining_pixels -= 1
+            #   continue
+            pass
+
+    nearest_body_part_indices = [black_pixels_body_part_dict[position] for position in contour_vertex_positions]
 
     return nearest_body_part_indices
