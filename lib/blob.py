d7b053fbf37c3b5d2f7062b6f5a78f9ff3f5a7dd
Arata Jingu <>
2018-11-09 21:03:07 +0900

    Add human blob error handling

--- lib/blob.py
+++ lib/blob.py
@@ -8,6 +8,10 @@ from lib.binarization import to_binary_inv_image
 def extract_human_blob(src):
     binary_inv = to_binary_inv_image(src)
     number_of_labels, lab = cv2.connectedComponents(binary_inv)
+
+    if number_of_labels < 2:
+        return None
+
     hist, _ = np.histogram(lab.flatten(), bins=number_of_labels - 1, range=(1, number_of_labels - 1))
     max_area_label = hist.argmax() + 1
     lab = np.where(lab == max_area_label, 0, 255)
