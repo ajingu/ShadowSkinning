4499c91593f715f3c661965586f9b268bf3c14b1
Arata Jingu <>
2018-11-15 02:06:51 +0900

    remove unused lines

--- lib/watch.py
+++ lib/watch.py
@@ -85,11 +85,6 @@ class ImageGenerationEventHandler(PatternMatchingEventHandler):
                               shadow.vertex_positions[pt1_index],
                               shadow.vertex_positions[pt2_index],
                               shadow.vertex_positions[pt3_index])
-
-            cv2.imwrite("./watch_test_images/adjust_skeleton2.jpg", polygons_image)
-            cv2.imshow("augmented_polygons", polygons_image)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
         except Exception:
             traceback.print_exc()
             pass
